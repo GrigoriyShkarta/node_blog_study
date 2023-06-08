effe80f99029251d5de5d2150cc1e26205c25451
Grigoriy <grishkoshkarta1@gmail.com>
2023-06-08 20:39:23 +0300

    day 3

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -1,4 +1,96 @@
 import PostModel from "../models/Post.js";
+import mongoose from "mongoose";
+
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+
+        res.json(posts)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'invalid get posts'
+        })
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const doc = await PostModel.findOneAndUpdate(
+            { _id: postId },
+            { $inc: { viewsCount: 1 } },
+            { returnDocument: 'after' }
+        );
+
+        if (!doc) {
+            return res.status(404).json({
+                message: "message not found"
+            });
+        }
+
+        res.json(doc);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'invalid get posts'
+        })
+    }
+}
+
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const doc = PostModel.findOneAndRemove({_id: postId});
+
+        if (!doc) {
+            return res.status(404).json({
+                message: "post not found"
+            });
+        }
+
+        res.json({
+            success: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'invalid get posts'
+        })
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        const postId =  new mongoose.Types.ObjectId(req.params.id);
+        const updatePost = {
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags
+        }
+
+        const doc = await PostModel.findOneAndUpdate(postId, updatePost);
+
+        if (!doc) {
+            return res.status(404).json({
+                message: "post not found"
+            });
+        }
+
+        await doc.save()
+
+        res.json({
+            success: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'invalid get posts'
+        })
+    }
+}
 
 export const create = async (req, res) => {
     try {
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import multer from 'multer';
 import mongoose from "mongoose";
+import path from "path";
+import cors from "cors";
 
-import {registerValidation, loginValidation} from "./validation/validations.js";
-import checkAuth from "./utils/checkAuth.js";
-
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
+import {registerValidation, loginValidation, postCreateValidation} from "./validation/validations.js";
+import {checkAuth, handleValidationErrors} from "./utils/index.js";
+import {UserController, PostController} from "./controllers/index.js"
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.eapn5gq.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB ok'))
@@ -13,13 +14,38 @@ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.eapn5gq.mongodb.net/blog?r
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Папка, в которую будут сохранены файлы
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+});
 
-app.post('/auth/login', loginValidation, UserController.login)
-app.post('/auth/register', registerValidation, UserController.register);
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/posts', checkAuth, PostController.create);
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 
 app.listen(4444, (err)=> {
     if (err) {
